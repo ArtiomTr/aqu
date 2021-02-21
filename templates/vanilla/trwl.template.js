fb36320d48db301e19d90327a595f531b89cfc77
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-02-21 22:52:09 +0200

    Created javascript template

---
+++ templates/vanilla/trwl.template.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('../../src/typings').TemplateScript}
+ */
+const script = {
+    initialize: () => ({
+        extend: "_base",
+        templateFilePaths: ["./example/src/index.js"],
+    }),
+};
+
+export default script;
