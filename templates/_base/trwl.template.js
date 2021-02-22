4ef02bfd42a7e0ca4cdb0f111b34c31c8b379885
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-02-22 13:15:44 +0200

    Created github actions, templates, added size-limit

--- templates/_base/trwl.template.js
+++ templates/_base/trwl.template.js
@@ -1,18 +1,27 @@
-import packageVersion from "../_utils/packageVersion";
+import packageVersion from '../_utils/packageVersion';
 
 /**
  * @type {import('../../src/typings').TemplateScript}
  */
 const templateScript = {
     initialize: async () => ({
-        templateFilePaths: ["package.json", "./example/README.md", "./example/package.json", "README.md"],
+        templateFilePaths: [
+            'package.json',
+            './example/README.md',
+            './example/package.json',
+            'README.md',
+        ],
         customArgs: {
             // TODO: replace with normal when published
-            ["versions.trwl"]: "file:../..",
+            ['versions.trwl']: 'file:../..',
             // ["versions.trwl"]: packageVersion("trwl"),
-            ["versions.np"]: await packageVersion("np"),
-            ["versions.lintStaged"]: await packageVersion("lint-staged"),
-            ["versions.husky"]: await packageVersion("husky"),
+            ['versions.np']: await packageVersion('np'),
+            ['versions.lintStaged']: await packageVersion('lint-staged'),
+            ['versions.husky']: await packageVersion('husky'),
+            ['versions.sizeLimit']: await packageVersion('size-limit'),
+            ['versions.sizeLimitPresetSmallLib']: await packageVersion(
+                '@size-limit/preset-small-lib',
+            ),
         },
     }),
 };
