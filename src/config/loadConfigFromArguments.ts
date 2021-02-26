7a2c7d8a811885dee5f0e43aee4147aa87562350
Artiom Tretjakovas <44021713+ArtiomTr@users.noreply.github.com>
2021-02-26 10:04:00 +0200

    Add possibility to specify build option via cli args (#23)
    * Added possibility to specify build options
    
    * externalNodeModules bug fix

--- src/config/loadConfigFromArguments.ts
+++ src/config/loadConfigFromArguments.ts
@@ -5,9 +5,16 @@ import { AquOptions } from '../typings';
 export const loadConfigFromArguments = async (
   args: Record<string, unknown>,
 ) => {
+  args['externalNodeModules'] = !args['noExternal'];
+  delete args['noExternal'];
+
   const config = await (loadAndResolveAquConfig(
     typeof args.config === 'string' ? args.config : undefined,
   ) as Promise<Array<AquOptions>>);
 
-  return Promise.all(config.map((cfg) => verifyConfig(cfg)));
+  if (config.length === 0) {
+    return Promise.all([verifyConfig(args as AquOptions)]);
+  }
+
+  return Promise.all(config.map((cfg) => verifyConfig({ ...cfg, ...args })));
 };
